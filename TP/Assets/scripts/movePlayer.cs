4fb8be9b8fdb5f22a833c533e781d411ca7bfdf5
marine.richer <marine.richer@prune.sh>
2025-07-26 02:10:41 +0200

    animations

--- TP/Assets/scripts/movePlayer.cs
+++ TP/Assets/scripts/movePlayer.cs
@@ -2,23 +2,47 @@ using UnityEngine;
 
 public class movePlayer : MonoBehaviour
 {
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
-       if (Input.GetKey(KeyCode.W))
+        bool is_front_walking = false;
+        bool is_back_walking = false;
+        bool is_left_walking = false;
+        bool is_right_walking = false;
+
+        if (Input.GetKey(KeyCode.W))
         {
+            is_front_walking = true;
             transform.Translate(0, 0, 15 * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.S))
         {
+            is_back_walking = true;
             transform.Translate(0, 0, -15 * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.D))
         {
+            is_right_walking = true;
             transform.Translate(15 * Time.deltaTime, 0, 0);
         }
         else if (Input.GetKey(KeyCode.A))
         {
+            is_left_walking = true;
             transform.Translate(-15 * Time.deltaTime, 0, 0);
-        }  
+        }
+        animator.SetBool("is_front_walking", is_front_walking);
+        animator.SetBool("is_back_walking", is_back_walking);
+        animator.SetBool("is_left_walking", is_left_walking);
+        animator.SetBool("is_right_walking", is_right_walking);
+        
     }
+    public void NewEvent()
+    {
+    }
+
 }
